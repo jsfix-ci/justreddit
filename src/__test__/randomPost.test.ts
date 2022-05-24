679b34bb7ae632d3560d1be75cd1b86f87f6e076
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2022-05-24 00:00:11 -0500

    fix test names

--- src/__test__/randomPost.test.ts
+++ src/__test__/randomPost.test.ts
@@ -1,6 +1,6 @@
 import { randomPost } from "../functions/randomPost";
 
-test("randomSub", async () => {
-    const sub = await randomPost({ sortType: "top", postGetLimit: 1 });
-    expect(sub).toBeDefined();
+test("randomPost", async () => {
+    const post = await randomPost({ sortType: "top", postGetLimit: 1 });
+    expect(post).toBeDefined();
 });
