a7b2d460833172e238e423d4cdb183298afd332e
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2022-05-24 00:01:46 -0500

    fixed randomImage functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { randomPostFromSub } from "./functions/randomPostFromSub";
 import { randomPost } from "./functions/randomPost";
 import { randomSub } from "./functions/randomSub";
+import { randomImage } from "./functions/randomImage";
+import { randomImageFromSub } from "./functions/randomImageFromSub";
 
-export { randomPostFromSub, randomPost, randomSub };
+export { randomPostFromSub, randomPost, randomSub, randomImage, randomImageFromSub };
